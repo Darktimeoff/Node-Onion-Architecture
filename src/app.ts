b8960a2ee4dd0b3473b2a7ca0940d589b3a332a1
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-19 21:41:48 +0300

    add linters

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express } from 'express';
 import { Server } from 'http';
 import { inject, injectable } from 'inversify';
 import { IExceptionFilter } from './errors/exception.filter.interface';
@@ -8,40 +8,40 @@ import 'reflect-metadata';
 import { IUserController } from './users/users.controller.interface';
 @injectable()
 export class App {
-    private port: number = 8000;
-    private host: string = '127.0.0.1';
-    private app: Express;
-    private server: Server;
+  private port = 8000;
+  private host = '127.0.0.1';
+  private app: Express;
+  private server: Server;
 
-    constructor(
-        @inject(TYPES.ILogger) private logger: ILogger,
-        @inject(TYPES.UserController) private usersController: IUserController,
-        @inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter
-    ) {
-        this.app = express()
-    }
+  constructor(
+    @inject(TYPES.ILogger) private logger: ILogger,
+    @inject(TYPES.UserController) private usersController: IUserController,
+    @inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
+  ) {
+    this.app = express();
+  }
 
-    public async init() {
-        this.useMiddleware();
+  public async init(): Promise<void> {
+    // this.useMiddleware();
 
-        this.useRoutes();
+    this.useRoutes();
 
-        this.useExceptionFilter();
+    this.useExceptionFilter();
 
-        this.server = this.app.listen(this.port, this.host, () => {
-            this.logger.log(`Server listen:http://${this.host}:${this.port}`)
-        })
-    }
+    this.server = this.app.listen(this.port, this.host, () => {
+      this.logger.log(`Server listen:http://${this.host}:${this.port}`);
+    });
+  }
 
-    useRoutes() {
-        this.app.use('/users', this.usersController.router)
-    }
+  useRoutes(): void {
+    this.app.use('/users', this.usersController.router);
+  }
 
-    useMiddleware() {
+  // useMiddleware(): void  {
 
-    }
+  // }
 
-    useExceptionFilter() {
-        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter))
-    }
+  useExceptionFilter(): void {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+  }
 }
